57ed7db7a5e89193693966c181bb40fc1951749b
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2016-02-23 18:58:09 +0000

    Removed unused code and added tradingDates list to equity class with some helper method

--- src/Portfolio/Equity.java
+++ src/Portfolio/Equity.java
@@ -2,14 +2,8 @@ package Portfolio;
 
 import QuandlAPIUser.DataFetcher;
 import QuandlAPIUser.Dataset;
-import QuandlAPIUser.JsonWrapper;
 import com.google.common.collect.Lists;
-import com.google.gson.Gson;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -32,6 +26,7 @@ public class Equity {
     private final List<Double> adjustedHighPrices;
     private final List<Double> adjustedLowPrices;
     private final List<Long> adjustedVolume;
+    private final List<Date> tradingDates;
 
     public Equity(String fullName, String ticker) {
         Dataset dataset = DataFetcher.getDataset(ticker);
@@ -45,6 +40,12 @@ public class Equity {
         this.adjustedHighPrices = createValuesList(dataset, "Adj. High");
         this.adjustedLowPrices = createValuesList(dataset, "Adj. Low");
         this.adjustedVolume = createValuesList(dataset, "Adj. Volume");
+        this.tradingDates = createTradingDatesList(dataset);
+    }
+
+    @Override
+    public String toString() {
+        return fullName;
     }
 
     private Date computeDate(String dateString) {
@@ -76,29 +77,23 @@ public class Equity {
         return values;
     }
 
-    @Override
-    public String toString() {
-        return fullName;
+    private List<Date> createTradingDatesList(Dataset dataset) {
+        List<String> tradingDatesAsString = createValuesList(dataset, "Date");
+        List<Date> tradingDates = new ArrayList<>();
+
+        for (String tradingDateAsString : Lists.reverse(tradingDatesAsString)) {
+            tradingDates.add(computeDate(tradingDateAsString));
+        }
+
+        return tradingDates;
     }
 
-//    public double getCurrentPrice() {
-//        double price = 0.0;
-//        String url = "http://download.finance.yahoo.com/d/quotes.csv?s=" + ticker + "&f=l1";
-//
-//        try {
-//            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-//            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//
-//            String line = in.readLine();
-//
-//            in.close();
-//
-//            price = Double.parseDouble(line);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        return price;
-//    }
+    private int dateToIndex(Date date) {
+        return tradingDates.indexOf(date);
+    }
+
+    private Date indexToDate(int index) {
+        return tradingDates.get(index);
+    }
 
 }
